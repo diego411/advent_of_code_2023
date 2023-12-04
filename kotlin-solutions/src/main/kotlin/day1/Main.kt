4de5b15a6f97258458ecc09bd9088c0b84352f76
diego411 <diego.enrico@directbox.com>
2023-12-04 21:39:39 +0100

    kotlin: part 2 day 1

--- kotlin-solutions/src/main/kotlin/day1/Main.kt
+++ kotlin-solutions/src/main/kotlin/day1/Main.kt
@@ -5,15 +5,29 @@ import java.io.File
 fun main() {
     var sum = 0
     File("./src/main/kotlin/day1/input.txt").forEachLine {
-        val (first, last) = getFirstAndLastNumber(it)
+        val code = replaceCharNumbers(it)
+        val (first, last) = getFirstAndLastNumber(code)
         if (first == null || last == null)
             throw IllegalArgumentException("Line did not contain a digit")
-
         val calibrationValue = "" + first + last
         sum += calibrationValue.toInt()
     }
 
     println(sum)
+}
+
+fun replaceCharNumbers(str: String): String {
+    var out = str.replace("one", "o1e")
+    out = out.replace("two", "t2o")
+    out = out.replace("three", "t3e")
+    out = out.replace("four", "f4r")
+    out = out.replace("five", "f5e")
+    out = out.replace("six", "s6x")
+    out = out.replace("seven", "s7n")
+    out = out.replace("eight", "e8t")
+    out = out.replace("nine", "m9e")
+
+    return out
 }
 
 fun getFirstAndLastNumber(str: String): Pair<Char?, Char?> {
